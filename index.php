b45f6295d29ac24818a3dd9d219d5bb9eb87ba27
Mikel3010 <mikelgremi30@gmail.com>
2023-04-11 21:53:50 +0200

    milestone1

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+    function randomPassword($lunghezza){
+        $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}[];,.<>?';
+        $password = '';
+        for ($i = 0; $i < $lunghezza; $i++){
+            $index = rand(0, strlen($caratteri) - 1);
+            $password .= $caratteri[$index];
+        }
+        return $password;
+    }
+    if (isset($_GET['lunghezza'])) {
+        $lunghezza = $_GET['lunghezza'];
+        $password = randomPassword($lunghezza);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+<body>
+    <h1>Passweord Casuale</h1>
+    <form action="index.php" method="GET">
+        <label for="lunghezza">Lunghezza Password</label>
+        <input type="number" name="lunghezza" id="lunghezza" min="1" max="20" required>
+        <button type="submit">Genera Password</button>
+    </form>
+    <?php echo "La tua password di lunghezza $lunghezza è : $password"?>
+</body>
+</html>
